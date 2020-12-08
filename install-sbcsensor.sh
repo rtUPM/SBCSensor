a6db086ad3eebfd5da19977f0ba0f637e5da0fea
rtUPM <60708038+rtUPM@users.noreply.github.com>
2020-12-08 13:53:10 +0100

    Update install-sbcsensor.sh

--- install-sbcsensor.sh
+++ install-sbcsensor.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-SBCServerIP=172.16.54.14
-SBCSensorIP=172.16.54.15
-DIR=/home/pi/
+SBCServerIP=
+SBCSensorIP=
+DIR=/home/pi
 
 #Install packages
 sudo apt-get update; sudo apt-get install -y python3-pip python-smbus i2c-tools wiringpi git
@@ -13,8 +13,8 @@ sudo pip3 install smbus2 RPi.bme280 RPi.GPIO
 #Install and configure
 cd ${DIR}; git clone https://github.com/rtUPM/SBCSensor
 chmod u+x ${DIR}/SBCSensor/scripts/*
-
-
+mv ${DIR}/SBCSensor/scripts/checkSBCServer /etc/cron.d/
+sudo chown root:root /etc/cron.d/chekcSBCServer
 
 #Enable I2C Interface
 sudo sed -i 's/#dtparam=i2c_arm=on/dtparam=i2c_arm=on/' /boot/config.txt

90f3b1bf5e916826006a914ec7fde81c68159a07
rtUPM <60708038+rtUPM@users.noreply.github.com>
2020-12-08 19:27:16 +0100

    Update getHumidity.py

--- scripts/getHumidity.py
+++ scripts/getHumidity.py
@@ -9,17 +9,6 @@ bus = smbus2.SMBus(port)
 
 calibration_params = bme280.load_calibration_params(bus, address)
 
-# the sample method will take a single reading and return a
-# compensated_reading object
 data = bme280.sample(bus, address, calibration_params)
 
-# the compensated_reading class has the following attributes
-#print(data.id)
-#print(data.timestamp)
-#print(data.temperature)
-#print(data.pressure)
 print("%.1f" % data.humidity)
-
-# there is a handy string representation too
-#print(data)
-

acdda3a193bee4e9e6edfa2f308eb493c3c156ae
rtUPM <60708038+rtUPM@users.noreply.github.com>
2020-12-09 10:32:28 +0100

    Update getAllData.py

--- scripts/getAllData.py
+++ scripts/getAllData.py
@@ -5,13 +5,11 @@ import bme280
 import RPi.GPIO as GPIO
 import time
 
-
 port = 1
 address = 0x76
 bus = smbus2.SMBus(port)
 
 calibration_params = bme280.load_calibration_params(bus, address)
-
 data = bme280.sample(bus, address, calibration_params)
 
 GPIO.setmode(GPIO.BOARD)
